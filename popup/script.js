bb575c190a88a6ccaac20f78e701460381455282
ProgramerNila <programernila@gmail.com>
2022-08-24 09:32:55 +0600

    kj

--- popup/script.js
+++ popup/script.js
@@ -1,15 +1,12 @@
 let button = document.querySelectorAll( "button" );
 let btnArr = Array.from( button );
-let closeBtn = document.querySelector( ".close" );
- console.log(closeBtn);
 
 btnArr.map( ( item ) => {
   
-  console.log(item.dataset.close);
   let modalview = document.querySelector( item.dataset.target );
   let closeBtn = document.querySelector(item.dataset.close);
   
-
+  
   item.addEventListener("click", () => {
     modalview.style.transform = "scale(1)";
   });
